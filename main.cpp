8de7f15c3cc9e6d840371b39576b5ee618997fbe
vsaint1 <vsantos1.dev@gmail.com>
2023-12-18 00:29:41 -0300

    example using entity class w/ reversed sh1ts

--- main.cpp
+++ main.cpp
@@ -4,26 +4,34 @@ int main() {
   LOG("PID: %d", m_memory.getPid());
 
   const auto module_base = m_memory.getModuleBase(GAME_NAME);
-  uint32_t local_player = m_memory.read<uint32_t>(module_base.value() + 0x017E360);
-  if (local_player) {
-    int health = m_memory.read<int>(local_player + 0xEC);
-    std::string name = m_memory.read<std::string>(local_player + 0x205);
-
-    LOG("Entity %p, health %d, name %s", local_player, health, name.c_str());
-  }
+  uint32_t local_player = m_memory.read<uint32_t>(module_base.value() + 0x017E254);
 
   uint32_t entity_list = m_memory.read<uint32_t>(module_base.value() + 0x18AC04);
   int player_count = m_memory.read<int>(module_base.value() + 0x18AC0C);
   LOG("Entity list %p, player count %d", entity_list, player_count);
 
-  for (uint32_t i = 0; i < 4; i++) {
-    uint32_t entity = m_memory.read<uint32_t>(entity_list + ( i * 0x4));
-    if (!entity)
+  while (!GetAsyncKeyState(VK_END)) {
+    Entity *entity_local = m_entity.player(local_player);
+    if (!entity_local->isAlive())
       continue;
 
-    int health = m_memory.read<int>(entity + 0xEC);
-    std::string name = m_memory.read<std::string>(entity + 0x205);
-    LOG("Entity %p, health %d, name %s", entity, health, name.c_str());
+    for (int i = 0; i < player_count; i++) {
+      uint32_t entity = m_memory.read<uint32_t>(entity_list + (i * 0x4));
+      if (!entity)
+        continue;
+
+      Entity *player = m_entity.player(entity);
+      if (!player)
+        continue;
+
+      if (player->isAlive()) {
+        LOG("Entity %p, Health %d , Armor %d, HeadPos %f %f %f, Position %f %f %f, ViewAngle %f %f %f, Name %s", player->getEntity(), player->getHealth(), player->getArmor(), player->getHeadPos().x,
+            player->getHeadPos().y, player->getHeadPos().z, player->getPosition().x, player->getPosition().y, player->getPosition().z, player->getViewAngles().x, player->getViewAngles().y,
+            player->getViewAngles().z, player->getName().c_str());
+      }
+    }
+
+    Sleep(1000);
   }
 
   return 0;
