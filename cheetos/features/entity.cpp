2b279c36c4934b20745bfff201b1a7e086eac14a
vsaint1 <vsantos1.dev@gmail.com>
2023-12-19 01:12:38 -0300

    reversed entity  weapon and inventory data

--- cheetos/features/entity.cpp
+++ cheetos/features/entity.cpp
@@ -1,36 +1,53 @@
 #include "../../pch.hpp"
 
+Entity *Entity::player(uint32_t address) {
 
-Entity* Entity::player(uint32_t address){
+  if (!address)
+    return nullptr;
 
+  this->entity = address;
 
-    if(!address)
-        return nullptr;
+  this->headPos = m_memory.read<Vector3>(address + offsets::head_pos);
+  this->position = m_memory.read<Vector3>(address + offsets::position);
+  this->viewAngles = m_memory.read<Vector3>(address + offsets::view_angles);
+  this->health = m_memory.read<uint32_t>(address + offsets::health);
+  this->armor = m_memory.read<uint32_t>(address + offsets::armor);
+  this->secondaryClipAmmo = m_memory.read<uint32_t>(address + offsets::secondary_clip_ammo);
+  this->primaryClipAmmo = m_memory.read<uint32_t>(address + offsets::primary_clip_ammo);
+  this->secondaryCurrentAmmo = m_memory.read<uint32_t>(address + offsets::secondary_current_ammo);
+  this->primaryCurrentAmmo = m_memory.read<uint32_t>(address + offsets::primary_current_ammo);
+  this->grenade = m_memory.read<uint32_t>(address + offsets::grenade);
+  this->knifeDelay = m_memory.read<uint32_t>(address + offsets::knife_delay);
+  this->secondaryGunWaitTime = m_memory.read<uint32_t>(address + offsets::secondary_gun_wait_time);
+  this->primaryGunWaitTime = m_memory.read<uint32_t>(address + offsets::primary_gun_wait_time);
+  this->secondaryShootsfired = m_memory.read<uint32_t>(address + offsets::secondary_shoots_fired);
+  this->primaryShootsFired = m_memory.read<uint32_t>(address + offsets::primary_shoots_fired);
+  this->killCount = m_memory.read<uint32_t>(address + offsets::kill_count);
+  std::string temp_name = m_memory.read<std::string>(address + offsets::name);
 
-    this->entity = address;
+  uint32_t inventory = m_memory.read<uint32_t>(address + offsets::inventory);
+  if (inventory) {
+    uint32_t weapon = m_memory.read<uint32_t>(inventory + 0xC);
+    if (weapon) {
 
-    this->headPos = m_memory.read<Vector3>(address + offsets::head_pos);
-    this->position = m_memory.read<Vector3>(address + offsets::position);
-    this->viewAngles = m_memory.read<Vector3>(address + offsets::view_angles);
-    this->health = m_memory.read<uint32_t>(address + offsets::health);
-    this->armor = m_memory.read<uint32_t>(address + offsets::armor);
-    this->secondaryClipAmmo = m_memory.read<uint32_t>(address + offsets::secondary_clip_ammo);
-    this->primaryClipAmmo = m_memory.read<uint32_t>(address + offsets::primary_clip_ammo);
-    this->secondaryCurrentAmmo = m_memory.read<uint32_t>(address + offsets::secondary_current_ammo);
-    this->primaryCurrentAmmo = m_memory.read<uint32_t>(address + offsets::primary_current_ammo);
-    this->grenade = m_memory.read<uint32_t>(address + offsets::grenade);
-    this->knifeDelay = m_memory.read<uint32_t>(address + offsets::knife_delay);
-    this->secondaryGunWaitTime = m_memory.read<uint32_t>(address + offsets::secondary_gun_wait_time);
-    this->primaryGunWaitTime = m_memory.read<uint32_t>(address + offsets::primary_gun_wait_time);
-    this->secondaryShootsfired = m_memory.read<uint32_t>(address + offsets::secondary_shoots_fired);
-    this->primaryShootsFired = m_memory.read<uint32_t>(address + offsets::primary_shoots_fired);
-    this->killCount = m_memory.read<uint32_t>(address + offsets::kill_count);
-    std::string temp_name = m_memory.read<std::string>(address + offsets::name);
+      std::string temp_weapon_type_str = m_memory.read<std::string>(weapon + 0x0);
+      if (temp_weapon_type_str.empty())
+        this->weapon_type_str = "Unknown_type";
+      else
+        this->weapon_type_str = temp_weapon_type_str;
 
-    if(temp_name.empty()) 
-        this->name_str = "Unknown";
-    else
-        this->name_str = temp_name;  
+      std::string temp_weapon_name_str = m_memory.read<std::string>(weapon + 0x17);
+      if (temp_weapon_name_str.empty())
+        this->weapon_name_str = "Unknown_name";
+      else
+        this->weapon_name_str = temp_weapon_name_str;
+    }
+  }
 
-    return this;
+  if (temp_name.empty())
+    this->name_str = "Unknown_player";
+  else
+    this->name_str = temp_name;
+
+  return this;
 }
--- cheetos/features/entity.hpp
+++ cheetos/features/entity.hpp
@@ -2,25 +2,22 @@
 #pragma once
 
 // the size isnt correct since i added some stuff to it
-
-class EntityList {
-public:
-  class Entity *entity; // 0x0000
-  char pad_0004[60];       // 0x0004
-};                         // Size: 0x0040
-static_assert(sizeof(N00000401) == 0x40);
-
 class Entity {
 
   uint32_t entity;
-  char pad_0000[4];             // 0x0000
-  Vector3 headPos;              // 0x0004
-  char pad_0010[24];            // 0x0010
-  Vector3 position;             // 0x0028
-  Vector3 viewAngles;           // 0x0034
-  char pad_0040[172];           // 0x0040
-  uint32_t health;              // 0x00EC
-  int32_t armor;                // 0x00F0
+  std::string name_str;
+  std::string weapon_type_str;
+  std::string weapon_name_str;
+  uint32_t inventoryAddress;
+
+  char pad_0000[4];              // 0x0000
+  Vector3 headPos;               // 0x0004
+  char pad_0010[24];             // 0x0010
+  Vector3 position;              // 0x0028
+  Vector3 viewAngles;            // 0x0034
+  char pad_0040[172];            // 0x0040
+  uint32_t health;               // 0x00EC
+  int32_t armor;                 // 0x00F0
   char pad_00F4[20];             // 0x00F4
   uint32_t secondaryClipAmmo;    // 0x0108
   char pad_010C[16];             // 0x010C
@@ -40,13 +37,15 @@ class Entity {
   char pad_0178[16];             // 0x0178
   uint32_t primaryShootsFired;   // 0x0188
   char pad_018C[80];             // 0x018C
-  int32_t killCount;            // 0x01DC
-  char pad_01E0[36];            // 0x01E0
-  char name[16];                // 0x0205
-  char pad_0208[352];           // 0x0208
-  class Inventory *Inventory;   // 0x0368
-  char pad_036C[240];           // 0x036C
-  std::string name_str;
+  int32_t killCount;             // 0x01DC
+  char pad_01E0[36];             // 0x01E0
+  char player_name[16];                 // 0x0205
+  char pad_0208[352];            // 0x0208
+  char pad_036C[240];            // 0x036C
+  char weapon_type[16];     // 0x0000
+  char pad_0004[12]; // 0x0004
+  char weapon_name[16];     // 0x0017
+  char pad_0014[48]; // 0x0014
 
 public:
   uint32_t getEntity() { return entity; }
@@ -63,28 +62,15 @@ public:
 
   Vector3 getViewAngles() { return viewAngles; }
 
-  bool isAlive() { return this->getHealth() > 0; }
+  std::string getWeaponType() { return weapon_type_str; }
+
+  std::string getWeaponName() { return weapon_name_str; }
+
+
+  bool isAlive() { return this->getHealth() > 0 && this->getHealth() <= 100; }
 
   Entity *player(uint32_t address);
 
 }; // Size: 0x045C
-static_assert(sizeof(Entity) == 0x45C);
-
-class Inventory {
-public:
-  char pad_0000[12];    // 0x0000
-  class Weapon *weapon; // 0x000C
-  char pad_0010[52];    // 0x0010
-};                      // Size: 0x0044
-static_assert(sizeof(N00000535) == 0x44);
-
-class Weapon {
-public:
-  char type[16];     // 0x0000
-  char pad_0004[12]; // 0x0004
-  char name[16];     // 0x0017
-  char pad_0014[48]; // 0x0014
-};                   // Size: 0x0044
-static_assert(sizeof(Weapon) == 0x44);
 
 inline Entity m_entity{};
