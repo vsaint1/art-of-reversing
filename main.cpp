2294b969b4686e64bea509040c62b33d196e91de
vsaint1 <vsantos1.dev@gmail.com>
2023-12-19 01:31:39 -0300

    added localplayer data logs

--- main.cpp
+++ main.cpp
@@ -26,9 +26,13 @@ int main() {
 
     if (!entity_local->isAlive())
       continue;
-    LOG("ViewAngle %f %f %f", entity_local->getViewAngles().x, entity_local->getViewAngles().y, entity_local->getViewAngles().z);
     ViewMatrix_t local_view_matrix = m_memory.read<ViewMatrix_t>(module_base.value() + offsets::view_matrix);
-    LOG("ViewMatrix %f %f %f %f", local_view_matrix.matrix[0][0], local_view_matrix.matrix[0][1], local_view_matrix.matrix[0][2], local_view_matrix.matrix[0][3]);
+    
+    LOG("LocalPlayer %p, Health %d , Armor %d, HeadPos %f %f %f, Position %f %f %f, ViewAngle %f %f %f, Name %s, WeaponType %s, WeaponName %s, ViewMatrix %f %f %f %f \n", entity_local->getEntity(),
+        entity_local->getHealth(), entity_local->getArmor(), entity_local->getHeadPos().x, entity_local->getHeadPos().y, entity_local->getHeadPos().z, entity_local->getPosition().x,
+        entity_local->getPosition().y, entity_local->getPosition().z, entity_local->getViewAngles().x, entity_local->getViewAngles().y, entity_local->getViewAngles().z,
+        entity_local->getName().c_str(), entity_local->getWeaponType().c_str(), entity_local->getWeaponName().c_str(), local_view_matrix.matrix[0][0], local_view_matrix.matrix[0][1],
+        local_view_matrix.matrix[0][2], local_view_matrix.matrix[0][3]);
 
     for (int i = 0; i < player_count; i++) {
       uint32_t entity = m_memory.read<uint32_t>(entity_list + (i * 0x4));
