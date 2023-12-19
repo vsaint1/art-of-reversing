5b13d9588a0b6038bfe68916329846e95c2ecaff
vsaint1 <vsantos1.dev@gmail.com>
2023-12-19 00:09:21 -0300

    stuff reversed entity  + weapon class

--- cheetos/features/entity.hpp
+++ cheetos/features/entity.hpp
@@ -1,17 +1,26 @@
 // Created with vsantos1.NET 1.2 by vsantos1
 #pragma once
 
+// the size isnt correct since i added some stuff to it
+
+class EntityList {
+public:
+  class Entity *entity; // 0x0000
+  char pad_0004[60];       // 0x0004
+};                         // Size: 0x0040
+static_assert(sizeof(N00000401) == 0x40);
+
 class Entity {
-  uint32_t entity;               // 0x0000
-  char pad_0000[4];              // 0x0000
-  Vector3 headPos;               // 0x0004
-  char pad_0010[32];             // 0x0010
-  Vector3 position;              // 0x0030
-  char pad_003C[8];              // 0x003C
-  Vector3 viewAngles;            // 0x0044
-  char pad_004C[160];            // 0x004C
-  uint32_t health;               // 0x00EC
-  uint32_t armor;                // 0x00F0
+
+  uint32_t entity;
+  char pad_0000[4];             // 0x0000
+  Vector3 headPos;              // 0x0004
+  char pad_0010[24];            // 0x0010
+  Vector3 position;             // 0x0028
+  Vector3 viewAngles;           // 0x0034
+  char pad_0040[172];           // 0x0040
+  uint32_t health;              // 0x00EC
+  int32_t armor;                // 0x00F0
   char pad_00F4[20];             // 0x00F4
   uint32_t secondaryClipAmmo;    // 0x0108
   char pad_010C[16];             // 0x010C
@@ -31,13 +40,15 @@ class Entity {
   char pad_0178[16];             // 0x0178
   uint32_t primaryShootsFired;   // 0x0188
   char pad_018C[80];             // 0x018C
-  uint32_t killCount;            // 0x01DC
-  char pad_01E0[2680];           // 0x01E0
-  char name[16];                 // 0x0205
+  int32_t killCount;            // 0x01DC
+  char pad_01E0[36];            // 0x01E0
+  char name[16];                // 0x0205
+  char pad_0208[352];           // 0x0208
+  class Inventory *Inventory;   // 0x0368
+  char pad_036C[240];           // 0x036C
   std::string name_str;
 
 public:
-  
   uint32_t getEntity() { return entity; }
 
   int getHealth() { return health; }
@@ -51,11 +62,29 @@ public:
   Vector3 getHeadPos() { return headPos; }
 
   Vector3 getViewAngles() { return viewAngles; }
-  
+
   bool isAlive() { return this->getHealth() > 0; }
 
-  Entity* player(uint32_t address);
-  
-};
+  Entity *player(uint32_t address);
+
+}; // Size: 0x045C
+static_assert(sizeof(Entity) == 0x45C);
+
+class Inventory {
+public:
+  char pad_0000[12];    // 0x0000
+  class Weapon *weapon; // 0x000C
+  char pad_0010[52];    // 0x0010
+};                      // Size: 0x0044
+static_assert(sizeof(N00000535) == 0x44);
+
+class Weapon {
+public:
+  char type[16];     // 0x0000
+  char pad_0004[12]; // 0x0004
+  char name[16];     // 0x0017
+  char pad_0014[48]; // 0x0014
+};                   // Size: 0x0044
+static_assert(sizeof(Weapon) == 0x44);
 
 inline Entity m_entity{};
